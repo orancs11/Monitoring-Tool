10b781c32207ce4621aaf9670a4eac448c70ec4a
orancs11 <CosmosSpaceShip1@hotmail.com>
2026-02-18 19:38:49 +0300

    Environment Initialization

---
+++ src/main/java/com/Oran/Monitoring_Tool/MonitoringToolApplication.java
@@ -0,0 +1,13 @@
+package com.Oran.Monitoring_Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Oran/Monitoring_Tool/MonitoringToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oran.Monitoring_Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
