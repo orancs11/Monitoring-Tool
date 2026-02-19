96bc83257ef5d07d42dbcca2c282eb972a6a62f6
orancs11 <CosmosSpaceShip1@hotmail.com>
2026-02-19 21:55:02 +0300

    Fetches CPU and RAM usage using MXBean

--- src/main/java/com/Oran/Monitoring_Tool/Client/ClientMainn.java
+++ src/main/java/com/Oran/Monitoring_Tool/Client/ClientMainn.java
@@ -3,28 +3,35 @@ package com.Oran.Monitoring_Tool.Client;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.net.Inet4Address;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import com.sun.management.OperatingSystemMXBean;
 
 public class ClientMainn {
     public static void main(String[] args) throws IOException {
+        OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)  ManagementFactory.getOperatingSystemMXBean();
+        Object obj = new Object();
+
         String IPAddress = Inet4Address.getLocalHost().getHostAddress();
-        Socket socket = new Socket(IPAddress, 3000);
-        var in = new DataInputStream(System.in);
+        Socket socket = new Socket(IPAddress, 3000); // Request Connection
+
+        //var in = new DataInputStream(System.in);
         var out = new DataOutputStream(socket.getOutputStream());
 
-        String message = "";
 
-        while(!message.equals("Quit!")){
+        while(true){
             try{
-                message = in.readLine();
-                out.writeUTF(message);
+                Thread.sleep(1000);
+                double currCpuUsage = bean.getCpuLoad();
+                if(bean.getCommittedVirtualMemorySize() == -1) break;
+                double currMemoryUsage = bean.getTotalMemorySize() - bean.getFreeMemorySize();
+                out.writeUTF("Current CPU usage: " + currCpuUsage * 100 + "%");
+                out.writeUTF("Current memory usage: " + currMemoryUsage + "bytes");
             }
-            catch (IOException e){
+            catch (Exception e){
                 e.printStackTrace();
             }
         }
-
     }
 }
